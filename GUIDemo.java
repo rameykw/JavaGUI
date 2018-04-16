f8927e8d33024bebd0c5eecf86aea0095bb025c5
Ramey <rameykw@l14347.cs.jmu.edu>
2018-04-16 11:40:45 -0400

    added a Title Reset button into GUIDemo.java, allowing the title to be changed after the program's creation

--- GUIDemo.java
+++ GUIDemo.java
@@ -15,6 +15,7 @@ public class GUIDemo extends JFrame
     private JPanel panel;
     private JButton biggerButton;
     private JButton smallerButton;
+    private JButton titleButton;
 
     /**
      * Set up the application.
@@ -27,11 +28,14 @@ public class GUIDemo extends JFrame
         panel = new JPanel();
         biggerButton = new JButton("BIGGER");
         smallerButton = new JButton("SMALLER");
+        titleButton = new JButton("TITLE RESET");
         biggerButton.addActionListener(new ButtonHandler());
         smallerButton.addActionListener(new ButtonHandler());
+        titleButton.addActionListener(new ButtonHandler());
         add(panel);
         panel.add(biggerButton);
         panel.add(smallerButton);
+        panel.add(titleButton);
         setVisible(true);
     }
 
@@ -53,9 +57,12 @@ public class GUIDemo extends JFrame
             {
                 setSize(size.width + 10, size.height + 10);
             }
-            else
+            else if (e.getSource().equals(smallerButton))
             {
                 setSize(size.width - 10, size.height - 10);
+            } else 
+            {
+                setTitle("Title Reset");
             }
 
         }
